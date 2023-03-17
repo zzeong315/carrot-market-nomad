44af24a22a8d86d8b278b24038e90f4fea7ec3eb
zzeong <zzeong315@gamil.com>
2023-03-17 12:29:40 +0900

    feat: 토큰 생성

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,13 +1,35 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 
 
 async function handler(
-  req:NextApiRequest, res:NextApiResponse
+  req:NextApiRequest, res:NextApiResponse<ResponseType>
 ) {
-  console.log(req.body);
-  return res.status(200).end();
+  const { phone, email } = req.body;
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
+    },
+  })
+  console.log(token);
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
